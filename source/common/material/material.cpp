85f0e2bf45782c9cad942698e423f2da59bb8790
farah-moh <75073858+farah-moh@users.noreply.github.com>
2023-11-30 23:47:36 +0200

    Requirement 7 Done

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -8,6 +8,8 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
+        pipelineState.setup();
+        shader->use();
     }
 
     // This function read the material data from a json object
@@ -25,6 +27,11 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+
+        // Calling the parent (Material) setup function
+        Material::setup();
+        // Setting the tint uniform
+        shader->set("tint",tint);
     }
 
     // This function read the material data from a json object
@@ -39,6 +46,29 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+
+        // Calling the parent (TintedMaterial) setup function
+        TintedMaterial::setup();
+
+        // Setting the alpha threshold
+        shader->set("alphaThreshold",alphaThreshold);
+
+        /* Setting the active texture unit to unit 0, even tho it's the default value
+         it's better to explicitly set it */
+        glActiveTexture(GL_TEXTURE0);
+
+        /* Here we set the active texture unit to 0 then bind the texture to it if it's not null
+        otherwise, we unbind it
+        */
+        if(texture) texture->bind();
+        else Texture2D::unbind();
+
+        // Then we bind the sampler to unit 0 if it's not null, otherwise, we unbind it
+        if(sampler) sampler->bind(0);
+        else Sampler::unbind(0);
+
+        // Then we send 0 (the index of the texture unit we used above) to the "tex" uniform
+        shader->set("tex",0);
     }
 
     // This function read the material data from a json object
